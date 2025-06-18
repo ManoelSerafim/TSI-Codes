3daad5b91d453dd8d74f345acff4e6e61bf38586
Manoel.S <manoeldserafim@gmail.com>
2025-06-18 17:28:57 -0300

    Adiciona arquivos de configuração e binários para os projetos de matriz em C# e Portugol

---
+++ C#/Atvd_matriz/Atvd_matriz/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atvd_matriz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matriz = new int[2, 2];
+            int[,] result = new int[2, 2];
+            int maior = -1000;
+
+            for (int i = 0; i <= 1; i++)
+            {
+                for (int j = 0; j <= 1; j++)
+                {
+                    // Preenchendo a matriz com valores
+                    Console.Write($"Digite o valor para a posição [{i},{j}]: ");
+                    matriz[i, j] = Convert.ToInt32(Console.ReadLine());
+                    // Atribuindo o valor maior
+                    if (matriz[i, j] > maior)
+                    {
+                        maior = matriz[i, j];
+                    }
+                }
+
+            }
+
+            for (int i = 0; i <= 1; i++)
+            {
+                Console.Write("\n");
+                for (int j = 0; j <= 1; j++)
+                {
+                    // Calculando o resultado
+                    result[i, j] = (matriz[i, j] * maior);
+                    // Exibindo o resultado
+                    Console.Write($"Resultado[{i},{j}] = {result[i, j]} \t");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/Matriz_teste/Matriz_teste/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matriz_teste
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] x = new int[2, 5];
+            // Preenchendo a matriz com valores
+            for (int i = 0; i <=1; i++)
+            {
+                for (int j = 0; j <= 4; j++)
+                {
+                    Console.Write($"Digite o valor para a posição [{i},{j}]: ");
+                    x[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            // Exibindo a matriz
+            for (int i = 0; i <= 1; i++)
+            {
+                Console.Write("\n");
+                for (int j = 0; j <= 4; j++)
+                {
+                    // Console.Write("x[" + i + "," + j + "] = " + x[i, j] + "\n");
+                    Console.Write($"X[{i},{j}] = {x[i, j]} \t");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
